b4ffd81dde54ee92b5f67b0fee50982bf8754151
Visgean Skeloru <me@visgean.me>
2017-01-07 16:13:34 +0100

    add fortune and ddate
    yes i am retarded

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y guake screen git-core htop steam indicator-multiload xclip redshift synaptic
+sudo apt-get install -y guake screen git-core htop steam indicator-multiload xclip redshift synaptic fortune-mod cowsay ddate
 sudo apt-get upgrade
 
 
