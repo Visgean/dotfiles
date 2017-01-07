7411657e1eccf42927be8b1b15d333e92fd02ed1
Visgean Skeloru <me@visgean.me>
2017-01-07 15:52:22 +0100

    uninstall bunch of unity shit

--- install/install.sh
+++ install/install.sh
@@ -1,2 +1,5 @@
-sudo apt-get install -y guake screen git-core htop steam indicator-multiload xclip
-        
+sudo apt-get update
+sudo apt-get install -y guake screen git-core htop steam indicator-multiload xclip redshift synaptic
+sudo apt-get upgrade
+
+
---
+++ install/uninstall_spam.sh
@@ -0,0 +1,20 @@
+sudo apt-get remove zeitgeist-core \
+	unity-scope-calculator \
+	unity-scope-chromiumbookmarks \
+	unity-scope-colourlovers \
+	unity-scope-devhelp \
+	unity-scope-firefoxbookmarks \
+	unity-scope-gdrive \
+	unity-scope-manpages \
+	unity-scope-openclipart \
+	unity-scope-texdoc \
+	unity-scope-tomboy \
+	unity-scope-video-remote \
+	unity-scope-virtualbox \
+	unity-scope-yelp \
+	unity-scope-zotero \
+	shotwell \
+	unity-lens-photos \
+	unity-lens-music \
+	unity-lens-video \
+	unity-webapps-service
