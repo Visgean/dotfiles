8aba6d3a2a1a4e4d366bdd689b8748c1c2cd5eb0
Visgean Skeloru <me@visgean.me>
2017-01-21 16:47:14 +0000

    install docker please

---
+++ install/install_docker.sh
@@ -0,0 +1,15 @@
+sudo apt-get install apt-transport-https ca-certificates
+
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+
+
+sudo add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+
+
+
+sudo apt-get update
+sudo apt-get -y install docker-engine
+
+
+sudo pip3 install docker-compose
+
