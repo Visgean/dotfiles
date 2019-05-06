f4ce3a8956803284c7abc064c152df78891a718b
Visgean Skeloru <me@visgean.me>
2019-05-06 12:50:37 +0100

    install darktable from ppa

---
+++ install/darktable.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:pmjdebruijn/darktable-release
+sudo apt-get update
+
+sudo apt install darktable
--- install_all.sh
+++ install_all.sh
@@ -7,3 +7,5 @@ bash ./install/base.sh
 bash ./load_dconf.sh
 bash ./move_configs.sh
 bash ./install/spotify.sh
+bash ./install/darktable.sh
+bash ./install/latex.sh
