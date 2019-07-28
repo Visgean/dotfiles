6ed3a018563cdf7a5b98ccdeaf5e69739bd711d2
Martin <me@visgean.me>
2019-07-28 13:55:16 +0100

    add command to diff configs

---
+++ diff_configs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+git diff configs/bashrc ~/.bashrc
+git diff configs/gitconfig ~/.gitconfig
+git diff configs/screenrc ~/.screenrc
+git diff configs/user-dirs.dirs ~/.config/
+git diff configs/tmux.conf ~/.tmux.conf
