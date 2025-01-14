9ab2f4a16177430c1adaface3ce279b963bf2e5c
martin <Visgean@gmail.com>
2025-01-14 17:58:48 +0100

    add new stuff

---
+++ cp_configs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+cp ~/.bashrc configs/bashrc
+cp ~/.zshrc configs/zshrc
+cp ~/.gitconfig configs/gitconfig
+cp ~/.screenrc configs/screenrc
+cp ~/.tmux.conf configs/tmux.conf
--- link-configs.sh
+++ link-configs.sh
@@ -1,19 +1,10 @@
 #!/bin/sh
 
 
-rm ~/.bashrc
-rm ~/.gitconfig
-rm ~/.screenrc
-rm ~/.config/user-dirs.dirs
-rm ~/.tmux.conf
-rm ~/.zshrc
-
-
-ln configs/bashrc ~/.bashrc
-ln configs/gitconfig ~/.gitconfig
-ln configs/screenrc ~/.screenrc
-ln configs/user-dirs.dirs ~/.config/user-dirs.dirs
-ln configs/tmux.conf ~/.tmux.conf
-ln configs/zshrc ~/.zshrc
+ln -s configs/bashrc ~/.bashrc
+ln -s configs/zshrc ~/.zshrc
+ln -s configs/gitconfig ~/.gitconfig
+ln -s configs/screenrc ~/.screenrc
+ln -s configs/tmux.conf ~/.tmux.conf
 
 mkdir -p ~/p
